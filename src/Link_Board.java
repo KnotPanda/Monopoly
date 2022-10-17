89cb36ce79cad3b3f1956c727e44fad0246d5154
KnotPanda <111515899+KnotPanda@users.noreply.github.com>
2022-10-17 15:31:18 -0500

    Classes

---
+++ src/Link_Board.java
@@ -0,0 +1,15 @@
+public class Link_Board {
+    public String name;
+    public int price;
+    public boolean purchased;
+    public String owner;
+    public int rent;
+    public boolean isPurchasable;
+    public String special;
+    public int sellvalue;
+    public String occupiedBy;
+
+    public Link_Board(String name, int price, boolean purchased, String owner, int rent, boolean isPurchasable, String special, int sellvalue, String occupiedBy){
+
+    }
+}
